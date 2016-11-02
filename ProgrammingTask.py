983224a51fc5bc2121d74fb55e657503fd21a833
BBlazej <bbajzik@gmail.com>
2016-11-02 22:33:08 +0100

    Update ProgrammingTask.py

--- ProgrammingTask.py
+++ ProgrammingTask.py
@@ -10,46 +10,48 @@ class Producer(object):
         	self.queue = queue
 
 	def __call__(self, urls):
-    		for url in urls:
-       			page = requests.get(url)	
-        		queue.put(page.text)
-
+    	for url in urls:
+       		page = requests.get(url)
+         	queue.put(page.text)
+             
 class Consumer(object):
 	def __init__(self, queue):
         	self.queue = queue
 	def __call__(self):
-		threadLock.acquire()
+		
 		empty = False
 		while not empty:
 			try:
-				response = queue.get(True, 4)
+				response = queue.get(True, 1)
 				dom = html.fromstring(response)	
+				threadLock.acquire()
 				for link in dom.xpath('.//a'):
 					if link:
 						print link.get('href')
-						
+				threadLock.release()		
 			except Queue.Empty:
 				empty = True
-		threadLock.release()
+		
 
 if __name__ == '__main__':
 
-    threadLock = threading.Lock()
-
-    urls = ['http://www.yahoo.com', 'http://manadartblog.tumblr.com']
-    queue = Queue.Queue()
-
-    prod = Producer(queue)
-    pthread = threading.Thread(target=prod, args=[urls])
-  
-    con = Consumer(queue)
-    cthread = threading.Thread(target=con)
-	cthread2 = threading.Thread(target=con)
-
-    pthread.start()
-    cthread.start()
-	cthread2.start()
 	
-    pthread.join()
-    cthread.join()
-	cthread2.join()
+   threadLock = threading.Lock()
+
+   urls = ['http://www.sme.sk', 'http://www.pravda.sk','http://hasici-bystricany.webnode.sk','http://www.microstep.com','http://www.bbc.com','http://www.shmu.sk']
+   queue = Queue.Queue(10)
+
+   prod = Producer(queue)
+   pthread = threading.Thread(target=prod, args=[urls])
+  
+   con = Consumer(queue)
+   cthread = threading.Thread(target=con)
+   cthread2 = threading.Thread(target=con)
+
+   pthread.start()
+   cthread.start()
+   cthread2.start()
+	
+   pthread.join()
+   cthread.join()
+   cthread2.join()
